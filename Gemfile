6f1cf706e991739a5af9c80153d2637421a25ff8
Ferdinand Niedermann <ferdinand.niedermann@simplificator.com>
2016-05-09 13:47:26 +0200

    Improve bot to cache response and only run extraction if necessary

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'slack-ruby-bot'
+gem 'pry-byebug'
--- lunch-bot.rb
+++ lunch-bot.rb
@@ -1,15 +1,51 @@
 require 'slack-ruby-bot'
 
-class LunchBot < SlackRubyBot::Bot
-  def self.retrieve_menu
-    weekday_short = DateTime.now.strftime('%a').downcase[0..1]
-    @@lunch5_menu_output = `./lunch5-menu.rb #{weekday_short}`
+class LunchBotContext
+  attr_reader :lunch5_menu_output
+
+  def retrieve_menu
+    @lunch5_menu_output = `./lunch5-menu.rb #{weekday_today}`
+    @data_weekday = weekday_today
   end
 
-  command 'lunch5' do |client, data, match|
-    client.say(text: "Good day! Here is today's Lunch 5 menu: \n#{@@lunch5_menu_output}", channel: data.channel)
+  def data_current?
+    return false unless @data_weekday
+    @data_weekday == weekday_today
+  end
+
+  private
+
+  attr_reader :data_weekday
+
+  def weekday_today
+    DateTime.now.strftime('%a').downcase[0..1]
   end
 end
 
-LunchBot.retrieve_menu
+class LunchBot < SlackRubyBot::Bot
+  command 'lunch5' do |client, data, match|
+    operation = proc do
+      lunch_bot_context.retrieve_menu
+    end
+    callback = proc do |result|
+      self.output_menu(client, data)
+    end
+
+    unless lunch_bot_context.data_current?
+      client.say(text: 'Hold on, fetching the latest menu for you...', channel: data.channel)
+      EventMachine.defer(operation, callback)
+    else
+      self.output_menu(client, data)
+    end
+  end
+
+  def self.output_menu(client, data)
+    client.say(text: "Here is today's Lunch 5 menu: \n#{lunch_bot_context.lunch5_menu_output}", channel: data.channel)
+  end
+end
+
+def lunch_bot_context
+  @@lunch_bot_context ||= LunchBotContext.new
+end
+
 LunchBot.run
